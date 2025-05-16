cf3c726709a6164661a2a7e60abfa76324dd342a
m-sawanobori <m-sawanobori@sakura.ad.jp>
2025-05-16 11:59:01 +0900

    fix

--- www/yo/ls.php
+++ www/yo/ls.php
@@ -1,6 +1,3 @@
 <?php
-$cd = 'cd /home/';
-$output = shell_exec($cd);
-$ls_cmd = 'ls -la';
-$ls_output = shell_exec($ls_cmd);
-echo "$ls_output";
+$output = shell_exec('cd /home && ls -la');
+echo "$output";
